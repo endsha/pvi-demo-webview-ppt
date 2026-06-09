b608dcad53b451315ecba1322d0890ad0146d7f8
Alvin Nguyen <anh.ns.dev@gmail.com>
2026-06-09 23:13:51 +0700

    feat: add accident & medical section with inpatient-only date range

--- src/pages/claim-request/ClaimRequestPage.tsx
+++ src/pages/claim-request/ClaimRequestPage.tsx
@@ -1,6 +1,7 @@
 import { Form, message } from 'antd'
 import { ClaimRequestHeader } from './components/ClaimRequestHeader'
 import { InsuredPersonSection } from './components/InsuredPersonSection'
+import { AccidentMedicalSection } from './components/AccidentMedicalSection'
 import { INSURED_PERSON_DEFAULTS } from './mock-claim-request'
 import { DEFAULT_TREATMENT_TYPE, type ClaimRequestFormValues } from './claim-request-form-helpers'
 
@@ -40,6 +41,7 @@ export function ClaimRequestPage() {
         >
           <div className="flex flex-col gap-5">
             <InsuredPersonSection />
+            <AccidentMedicalSection />
           </div>
         </Form>
       </div>
